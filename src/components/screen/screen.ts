168cb5f33613d371a3923a288ce02d8f6790538e
Brett Fowler <b16fowler@gmail.com>
2026-01-28 11:48:23 -0500

    Component directory to display calcular screen

---
+++ src/components/screen/screen.ts
@@ -0,0 +1,27 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-screen',
+  imports: [],
+  template: `
+    <div>
+      <h1>
+        {{ onScreen() }}
+      </h1>  
+    </div>
+  `,
+  styles: `
+    div {
+      border-style: solid;
+      margin: auto;
+      margin-top: 3%;
+      margin-bottom: 3%;
+      font-size: 38px;
+      width: 40%;
+      align-self: center;
+    }
+  `,
+})
+export class Screen {
+  onScreen = input<number>();
+}
