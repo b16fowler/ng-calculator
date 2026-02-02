57a4e5d1f07c877fb88fa7c98b6fea384f67b9f9
Brett Fowler <b16fowler@gmail.com>
2026-02-02 14:10:45 -0500

    Combined handleSolve() into handleOperation() + checkRounding function to drop decimals for integers

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,7 +3,7 @@ import { Header } from '../components/header/header';
 import { Number } from '../components/buttons/number/number';
 import { Screen } from '../components/screen/screen';
 import { Clear } from '../components/buttons/clear/clear';
-import { Operation } from "../components/buttons/operation/operation";
+import { Operation } from '../components/buttons/operation/operation';
 
 @Component({
   selector: 'app-root',
@@ -15,27 +15,30 @@ import { Operation } from "../components/buttons/operation/operation";
       <app-number key="7" (clicked)="handleKey('7')" />
       <app-number key="8" (clicked)="handleKey('8')" />
       <app-number key="9" (clicked)="handleKey('9')" />
-      <app-operation key='+' (clicked)="handleOperation('+')"/>
+      <app-operation key="+" (clicked)="handleOperation('+')" />
+      <app-operation key="=" (clicked)="handleOperation('=')" />
     </div>
     <div class="keypad-row">
       <app-number key="4" (clicked)="handleKey('4')" />
       <app-number key="5" (clicked)="handleKey('5')" />
       <app-number key="6" (clicked)="handleKey('6')" />
-      <app-operation key='-' (clicked)="handleOperation('-')" />
+      <app-operation key="-" (clicked)="handleOperation('-')" />
+      <app-operation key=" " />
     </div>
     <div class="keypad-row">
       <app-number key="1" (clicked)="handleKey('1')" />
       <app-number key="2" (clicked)="handleKey('2')" />
       <app-number key="3" (clicked)="handleKey('3')" />
-      <app-operation key='*' (clicked)="handleOperation('*')" />
+      <app-operation key="*" (clicked)="handleOperation('*')" />
+      <app-operation key=" " />
     </div>
     <div class="keypad-row">
       <app-number key=" " />
       <app-number key="0" (clicked)="handleKey('0')" />
       <app-number key="." />
-      <app-operation key="/" (clicked)="handleOperation('/')"/>
+      <app-operation key="/" (clicked)="handleOperation('/')" />
+      <app-operation key=" " />
     </div>
-    <app-operation key="=" (clicked)="handleSolve()"/>
     <app-clear (clicked)="clear()"></app-clear>
   `,
   styles: [
@@ -54,7 +57,7 @@ import { Operation } from "../components/buttons/operation/operation";
   ],
 })
 export class App {
-  onScreen = signal("0");
+  onScreen = signal('0');
 
   // Global listener for any keydowns
   @HostListener('window:keydown', ['$event'])
@@ -66,53 +69,35 @@ export class App {
     }
   }
 
-  handleSolve() {
-    //TODO: USE LATER AS TO NOT USE EVAL()
-    // const split = this.onScreen().split(/\D/);
-    // const operation = this.onScreen().match(/\D/)?.[0];
-    
-    // const num1 = +split[0];
-    // const num2 = +split[1];
-    // console.log("onScreen(): ", this.onScreen());
-    // console.log("num1: ", num1);
-    // console.log("num2: ", num2);
-    // console.log("operation: ", operation);
-    
-    // eval solution (for now)
-    let solution = eval(this.onScreen());
-    solution = solution.toFixed(2);
-    this.handleRound(solution)
-    this.onScreen.set(solution);
-  }
-  
   //TODO ONLY ROUND TO 2 DECIMAL PLACES WHEN NOT INTEGER
   handleOperation(key: string) {
     let solution = eval(this.onScreen());
-    // solution = this.handleRound(solution);
-    if (key === "=") this.onScreen.set(solution)
+    solution = this.checkRounding(solution);
+    if (key === '=') this.onScreen.set(solution);
     else {
       this.onScreen.set(solution + key);
     }
   }
 
   handleKey(key: string) {
-    if (this.onScreen() === "0") this.onScreen.set(key);
+    if (this.onScreen() === '0') this.onScreen.set(key);
     else {
-      this.onScreen.update(val => val + key);
+      this.onScreen.update((val) => val + key);
     }
   }
 
-  handleRound(solution: number) {
-    console.log(solution);
-    // solution has none zero decimal
-    console.log(typeof solution);
-    console.log(typeof solution.toFixed(2));
-    
-    
+  checkRounding(solution: number) {
+    // If solution is integer, drop decimals
+    if (solution % 1 !== 0) {
+      const rounded = solution.toFixed(2);
+      let roundedStr = +rounded;
+      return roundedStr;
+    }
+    return solution;
   }
 
   // Reset onScreen to 0
   clear() {
-    this.onScreen.set("0");
+    this.onScreen.set('0');
   }
 }
